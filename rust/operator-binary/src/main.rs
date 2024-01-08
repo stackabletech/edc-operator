f28a7652fda9ae3776d5cb1bca79555255d9fffe
Nick Larsen <nick.larsen@stackable.tech>
2024-01-08 15:18:58 +0100

    print_yaml_schema needs the version str

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -39,7 +39,7 @@ struct Opts {
 async fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
-        Command::Crd => EDCCluster::print_yaml_schema()?,
+        Command::Crd => EDCCluster::print_yaml_schema(built_info::CARGO_PKG_VERSION)?,
         Command::Run(ProductOperatorRun {
             product_config,
             watch_namespace,
